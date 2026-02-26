303e84c8cc96d3dd744fd9127d004528b16546b2
Bipul Hf <bipulhf@gmail.com>
2026-02-26 12:28:57 +0600

    u/secured mail

--- src/common/services/email.service.ts
+++ src/common/services/email.service.ts
@@ -21,7 +21,7 @@ class EmailService {
     this.transporter = nodemailer.createTransport({
       host: process.env.EMAIL_HOST || "email-smtp.us-east-1.amazonaws.com",
       port: Number(process.env.EMAIL_PORT) || 465,
-      secure: true,
+      secure: process.env.EMAIL_SECURE === "true",
       auth: {
         user: process.env.EMAIL_USER!,
         pass: process.env.EMAIL_PASSWORD!,
